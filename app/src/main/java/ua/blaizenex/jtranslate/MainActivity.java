cb2553dea7093cc2bbc9c764c5291c4262c63d90
Yaroslav Ovdienko <jaroslaw.owdiyenko@ex.ua>
2016-05-17 10:32:09 +0300

    May 17 UI -> Main

--- app/src/main/java/ua/blaizenex/jtranslate/MainActivity.java
+++ app/src/main/java/ua/blaizenex/jtranslate/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener {
@@ -15,15 +17,27 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        spinner.setOnItemSelectedListener(this);
-// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.translate_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        final EditText edHandle_input = (EditText)findViewById(R.id.edHandle_input);
+
+        //Button clear text area block
+            Button btnClearTextArea = (Button)findViewById(R.id.btnClearTextArea);
+            btnClearTextArea.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    edHandle_input.setText("");
+                }
+            });
+        //Button clear text area block <END>
+
+        //Spinner block
+            Spinner spinner = (Spinner) findViewById(R.id.spinner);
+            spinner.setOnItemSelectedListener(this);
+
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                    R.array.translate_array, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+        //Spinner block <END>
     }
 
     @Override
