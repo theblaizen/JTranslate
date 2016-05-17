22bbfa03dc842f5fd981067169baa2d48bf35624
Yaroslav Ovdienko <jaroslaw.owdiyenko@ex.ua>
2016-05-17 17:11:34 +0300

    May 17 UI -> Main

--- app/src/main/java/ua/blaizenex/jtranslate/MainActivity.java
+++ app/src/main/java/ua/blaizenex/jtranslate/MainActivity.java
@@ -1,6 +1,8 @@
 package ua.blaizenex.jtranslate;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,15 +11,40 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener {
+
+    TextView tvTranslationTextField;
+    EditText edHandle_input ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText edHandle_input = (EditText)findViewById(R.id.edHandle_input);
+        tvTranslationTextField = (TextView)findViewById(R.id.tvTranslationTextField);
+        edHandle_input = (EditText)findViewById(R.id.edHandle_input);
+
+        //reading dict
+        try {
+            readToHashMap();
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(),
+                    "Problems: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
 
         //Button clear text area block
             Button btnClearTextArea = (Button)findViewById(R.id.btnClearTextArea);
@@ -28,6 +55,16 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
                 }
             });
         //Button clear text area block <END>
+
+        //Button view txt block
+        Button btnViewTXT = (Button)findViewById(R.id.btnViewTXT);
+        btnViewTXT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startSelection();
+            }
+        });
+        //Button view txt block <END>
 
         //Spinner block
             Spinner spinner = (Spinner) findViewById(R.id.spinner);
@@ -40,6 +77,14 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
         //Spinner block <END>
     }
 
+    private void startSelection() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_PICK);
+        Uri startDir = Uri.fromFile(new File("/sdcard"));
+        intent.setDataAndType(startDir,"*.txt");
+        startActivity(intent);
+    }
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
@@ -48,5 +93,24 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    public void readToHashMap() throws IOException {
+        Map<String, String> eng_por = new HashMap<String, String>();
+        Map<String, String> por_eng = new HashMap<String, String>();
+        String line = "";
+        String line2 = "";
+        InputStream is = this.getResources().openRawResource(R.raw.dict);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        if (is != null) {
+            while ((line = reader.readLine()) != null) {
+               line2 = reader.readLine();
+                eng_por.put(line,line2);
+                por_eng.put(line2,line);
+            }
+        }
+        is.close();
+        String res = por_eng.get("errôneo") + " : " + eng_por.get("erroneous");
+        tvTranslationTextField.setText(res);
     }
 }
